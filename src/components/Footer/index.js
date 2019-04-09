c99de75edd1545ea9707b59a18aa32d05e961548
hazratgs <hazratgs@gmail.com>
2019-04-09 19:04:36 +0300

    Add footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,10 +1,24 @@
 import React from 'react'
-import { Footer, Copyright } from './styles'
+import { Footer, Copyright, Nav, Link } from './styles'
 
 export default () => {
   const year = new Date().getFullYear()
   return (
     <Footer>
+      <Nav>
+        <Link target='_blank' href='https://github.com/hazratgs'>
+          <img src='/github.svg' alt='github'/>
+          <span>GitHub</span>
+        </Link>
+        <Link target='_blank' href='https://t.me/hazratgs'>
+          <img src='/telegram.png' alt='telegram'/>
+          <span>Telegram</span>
+        </Link>
+        <Link href='mailto:hazratgs@gmail.com'>
+          <img src='/arroba.svg' alt='email'/>
+          <span>Email</span>
+        </Link>
+      </Nav>
       <Copyright>© 2012–{year} Хазрат Гаджикеримов</Copyright>
     </Footer>
   )
--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -5,5 +5,30 @@ export const Footer = styled.div`
 `
 
 export const Copyright = styled.p`
+  font-size: 13px;
+  opacity: .4;
+`
 
+export const Nav = styled.div`
+  padding: 20px 0;
+  display: flex;
+`
+
+export const Link = styled.a`
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  color: #000;
+  margin-right: 25px;
+
+  img {
+    margin-right: 10px;
+    width: 24px;
+    height: 24px;
+  }
+
+  span {
+    text-decoration: underline;
+    font-size: 14px;
+  }
 `
