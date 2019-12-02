dea2b999c51e4858e33b80180d567fa4123e7cdb
hazratgs <hazratgs@gmail.com>
2019-12-02 15:16:16 +0300

    add expiriens

--- src/components/Experiens/index.js
+++ src/components/Experiens/index.js
@@ -16,11 +16,34 @@ export default () => (
     <Wrapper>
       <Item>
         <Company target='_blank' href='http://injoy.org/'>
+          <img src='https://docdoc.ru/static/new-doc-doc/favicon/favicon-32x32.png' alt='docdoc' />
+          docdoc
+        </Company>
+        <Type>Senior Frontend Developer</Type>
+        <DateWork>Сентябрь 2019 - По настоящее время</DateWork>
+        <Description>
+          <b>Основные обязаности:</b>
+          <p>• Проектирование архитектуры проектов</p>
+          <p>• Разработка приложений для телемедицины</p>
+          <p>• Покрытие тестами всего функционала</p>
+          <p>• Code Review</p>
+          <p>• Построение графиков с помощью Influxdb и Grafana</p>
+          <p>• Openvidu</p>
+          <p>• New Relic</p>
+          <p>• CI/CD</p>
+        </Description>
+        <Description>
+          <b>Используемые технологии:</b>
+          <p className='english'>TypeScript • React • Redux-Saga • Linaria • Node.js</p>
+        </Description>
+      </Item>
+      <Item>
+        <Company target='_blank' href='http://injoy.org/'>
           <img src='//u20.filesonload.ru/e49a25e4fab78006866e605c952cc2d6/57dd20997bd4dfcd302b8c0abc32c0bb.png' alt='injoy' />
           Injoy
         </Company>
         <Type>Senior Frontend Developer</Type>
-        <DateWork>Апрель 2019 - По настоящее время</DateWork>
+        <DateWork>Апрель 2019 - Август 2019</DateWork>
         <Description>
           <b>Основные обязаности:</b>
           <p>• Разработка и поддержка инновационного образовательного проекта по принципу Финского образования</p>
